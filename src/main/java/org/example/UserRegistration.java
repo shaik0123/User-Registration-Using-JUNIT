7e899d1b293522538ddc891e2dda67e1fe55a1fe
shaik0123 <shaik.ismail8563@gmail.com>
2023-03-07 19:31:55 +0530

    Lambda

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -1,15 +1,15 @@
 package org.example;
 
+import java.util.function.Predicate;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
     boolean result;
 
-    public boolean getFirstName(String firstName){
+    Predicate<String> userFirstName = firstName -> {
 
         result = firstName.matches("^[A-Z]{1}[a-z]{3,}");
-
         try{
             if(!result){
                 throw new UserValidationCustomException("Entered Firstname is Invalid");
@@ -19,71 +19,62 @@ public class UserRegistration {
         }
 
         return result;
+    };
 
-    }
-
-
-    public Boolean getLastName(String lastName) {
+    Predicate<String> userLastName = lastName -> {
 
         result = lastName.matches("^[A-Z]{1}[a-z]{3,}");
-
         try{
             if(!result){
-                throw new UserValidationCustomException("Entered Lastname is Invalid");
+                throw new UserValidationCustomException("Entered LastName is Invalid");
             }
         } catch (UserValidationCustomException e) {
             System.out.println(e.getMessage());
         }
 
         return result;
+    };
 
-    }
+    Predicate<String> userEmailId = emailId -> {
 
-    public Boolean getEmail(String email) {
-
-        result = email.matches("^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,4})([.][a-zA-Z]{2})*$");
-
+        result = emailId.matches("^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,4})([.][a-zA-Z]{2})*$");
         try{
             if(!result){
-                throw new UserValidationCustomException("Entered Email is Invalid");
+                throw new UserValidationCustomException("Entered Email Id is Invalid");
             }
         } catch (UserValidationCustomException e) {
             System.out.println(e.getMessage());
         }
 
         return result;
+    };
 
-    }
+    Predicate<String> userPhoneNumber = phoneNumber -> {
 
-    public Boolean getPhoneNumber(String phoneNumber) {
-
-        result = phoneNumber.matches("^[1-9]{2}\\s[0-9]{10}$");
-
+        result = phoneNumber.matches("^[1-9]{2}\\s[0-9]{10}");
         try{
             if(!result){
-                throw new UserValidationCustomException("Entered PhoneNumber is Invalid || Please Check");
+                throw new UserValidationCustomException("Entered Phone Number is Invalid");
             }
         } catch (UserValidationCustomException e) {
             System.out.println(e.getMessage());
         }
+
         return result;
+    };
 
-    }
-
-    public Boolean getPassword(String passWord) {
+    Predicate<String> userPassWord = passWord -> {
 
         result = passWord.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=.*[a-z]).{8,}$");
-
         try{
             if(!result){
-                throw new UserValidationCustomException("Entered PassWord is Invalid || Please Check");
+                throw new UserValidationCustomException("Entered Password is Invalid");
             }
         } catch (UserValidationCustomException e) {
             System.out.println(e.getMessage());
         }
 
         return result;
-
-    }
+    };
 
 }
